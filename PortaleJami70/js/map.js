35923388b81f83c48eb4e02d2a2b3364c39702f0
DNLMRT <120659950+DanieleMRTT@users.noreply.github.com>
2023-01-08 21:49:47 +0100

    sistemato bottone scroll e aggiunto nuovi bottoni

--- PortaleJami70/js/map.js
+++ PortaleJami70/js/map.js
@@ -228,13 +228,13 @@ document.getElementById('flexCheckWc').addEventListener('change',function(){
 if (this.checked){
   arraywc.forEach(function(element) {
     element.setMap(map)
-    console.log("siamo in checked")
+    
    });
 }
 else {
   arraywc.forEach(function(element) {
   element.setMap(null);
-  console.log("siamo in else")
+  
   });
 }
 });
@@ -266,7 +266,7 @@ service.nearbySearch(request, function(results, status) {
         icon: {url:'../img/attrazione2.png', scaledSize: new google.maps.Size(60, 60)},
 
       });
-      console.log(marker);
+      
       attractions.push(marker);
     }
  
@@ -278,13 +278,13 @@ document.getElementById('flexCheckInterestPoints').addEventListener('change',fun
   if (this.checked){
     attractions.forEach(function(element) {
       element.setMap(map)
-      console.log("siamo in checked")
+      
      });
   }
   else {
     attractions.forEach(function(element) {
     element.setMap(null);
-    console.log("siamo in else")
+    
     });
   }
   
@@ -328,25 +328,7 @@ $(window).on("scroll", function() {
 
 /*----fine jquery------ */
 
-//scroll to top btn
 
-// Get button:
-let mybutton = document.getElementById("myBtn");
 
-// When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function() {scrollFunction()};
 
-function scrollFunction() {
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    mybutton.style.display = "block";
-  } else {
-    mybutton.style.display = "none";
-  }
-}
-
-// When the user clicks on the button, scroll to the top of the document
-function topFunction() {
-  document.body.scrollTop = 0; // For Safari
-  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
-}
 
